5786cc0a602bda65ed1295004cb6da4fcd8e5178
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-10-29 00:53:38 -0300

    fixed alias

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,5 +7,5 @@ module.exports = function (grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('heroku', 'jshint');
+  grunt.registerTask('heroku', ['jshint']);
 };
